f4721d970cbdaf352eb633766b22acd9539b2fca
Gerardo_RJI <gera.rji96@gmail.com>
2026-06-25 11:17:44 -0600

    add functionality to edit profile and change password for the users

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -6,7 +6,9 @@ import {
   autenticar,
   olvidePassword,
   comprobarToken,
-  nuevoPassword
+  nuevoPassword,
+  actualizarPerfil,
+  actualizarPassword
 } from "../controllers/veterinarioController.js";
 import checkAuth from "../middleware/authMiddleware.js";
 
@@ -17,9 +19,11 @@ router.post('/', registrar);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
 router.post('/olvide-password', olvidePassword);
-router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword)
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
 
 // area privada
 router.get('/perfil',checkAuth, perfil);
+router.put('/perfil/:id', checkAuth, actualizarPerfil);
+router.put('/actualizar-password', checkAuth, actualizarPassword);
 
 export default router;
